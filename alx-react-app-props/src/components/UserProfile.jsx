31567bfdbb1f300cb1bd99ba534c2fdce8d9e5a4
lemmyszn <lemmyxking17@gmail.com>
2025-07-27 23:07:03 +0300

    update

--- alx-react-app-props/src/components/UserProfile.jsx
+++ alx-react-app-props/src/components/UserProfile.jsx
@@ -1,5 +1,5 @@
 import React, { useContext } from 'react';
-import { UserContext } from './src/UserContext';
+import  UserContext  from './src/UserContext';
 
 const UserProfile = (props) => {
   return (
