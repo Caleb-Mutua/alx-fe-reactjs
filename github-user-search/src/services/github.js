1055698b3259b0a06c18ef4bd0a840c529b5d35f
lemmyszn <lemmyxking17@gmail.com>
2025-08-01 17:03:33 +0300

    updated github.js for the github api calls

--- github-user-search/src/services/github.js
+++ github-user-search/src/services/github.js
@@ -1,13 +1,7 @@
-import axios from "axios";
+import axios from 'axios';
 
-const GITHUB_API = "https://api.github.com/users";
-
-export const fetchUser = async (username) => {
-  try {
-    const response = await axios.get(`${GITHUB_API}/${username}`);
-    return response.data;
-  } catch (error) {
-    console.error("Error fetching user:", error);
-    return null;
-  }
+export const fetchUserData = async (username) => {
+  const url = `https://api.github.com/users/${username}`;
+  const response = await axios.get(url);
+  return response.data;
 };
