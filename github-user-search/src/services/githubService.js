01387ced859af19e79537243bf74f0fb9ff0ee75
lemmyszn <lemmyxking17@gmail.com>
2025-08-02 14:41:56 +0300

    updated to include new features(location and minirepo)

--- github-user-search/src/services/githubService.js
+++ github-user-search/src/services/githubService.js
@@ -1,7 +1,15 @@
 import axios from 'axios';
 
-export const fetchUserData = async (username) => {
-  const url = `https://api.github.com/users/${username}`;
+export const fetchUsers = async ({ username, location, minRepos }) => {
+  let queryParts = [];
+
+  if (username) queryParts.push(`${username} in:login`);
+  if (location) queryParts.push(`location:${location}`);
+  if (minRepos) queryParts.push(`repos:>=${minRepos}`);
+
+  const query = queryParts.join(' ');
+  const url = `https://api.github.com/search/users?q=${encodeURIComponent(query)}`;
+
   const response = await axios.get(url);
-  return response.data;
+  return response.data.items;
 };
