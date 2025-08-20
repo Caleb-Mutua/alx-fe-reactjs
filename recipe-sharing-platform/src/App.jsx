0b36267353e1b143abbcfe4a343562c34c2e95a4
lemmyszn <lemmyxking17@gmail.com>
2025-08-20 15:56:42 +0300

    succcessfully added recipe detail page

--- recipe-sharing-platform/src/App.jsx
+++ recipe-sharing-platform/src/App.jsx
@@ -1,18 +1,19 @@
 import React from 'react';
+import './App.css';
+import './index.css';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import HomePage from './components/HomePage';
-import './App.css'
-import './index.css'
+import RecipeDetail from './components/RecipeDetail';
 
 function App() {
-  
   return (
-   
-      <div className="bg-gray-100 min-h-screen">
-       <HomePage/>
-      </div>
-      
-    
+    <Router>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/recipe/:id" element={<RecipeDetail />} />
+      </Routes>
+    </Router>
   );
 }
 
-export default App
+export default App;
